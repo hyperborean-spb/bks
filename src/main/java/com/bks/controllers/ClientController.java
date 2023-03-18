2bfe4cc16d5ceb1dd816acb30f8677e1fe30f764
stepanovam <stepanovam@c-i-systems.com>
2023-03-18 05:58:42 +0300

    добавлен SpringDoc для документирования API (начало)

--- src/main/java/com/bks/controllers/ClientController.java
+++ src/main/java/com/bks/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.bks.controllers;
 import com.bks.domain.Client;
 import com.bks.dto.ClientDto;
 import com.bks.service.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -17,33 +19,38 @@ import java.time.LocalDate;
 @RestController
 @RequestMapping("/api/client")
 @RequiredArgsConstructor
+@Tag(name="Клиентские операции")
 public class ClientController {
 
 	private final ClientService clientService;
 
-
 	@GetMapping("/getbyphone/{phone}")
+	@Operation(summary = "Поиск по телефону")
 	public ResponseEntity<Client> getClientByPhone(@PathVariable String phone) {
 		return ResponseEntity.ok().body(clientService.getClientByPhone(phone));
 	}
 
 	@GetMapping("/getbyname")
+	@Operation(summary = "Поиск по имени")
 	public ResponseEntity<Page<Client>> getClientByName(@RequestParam int page, @RequestParam int size, @RequestParam  String name) {
 		return ResponseEntity.ok().body(clientService.getClientsByName(page,  size,  name));
 	}
 
 	@GetMapping("/getbymail/{mail}")
+	@Operation(summary = "Поиск по адресу e-почты")
 	public ResponseEntity<Client> getClientByMail(@PathVariable String mail) {
 		return ResponseEntity.ok().body(clientService.getClientByMail(mail));
 	}
 
 	@GetMapping("/getbybirthdate")
+	@Operation(summary = "Поиск по дате рождения")
 	public ResponseEntity<Page<Client>> getClientByBirthdate(@RequestParam int page, @RequestParam int size, @RequestParam  @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate birthdate) {
 		return ResponseEntity.ok().body(clientService.getClientsByBirthdate(page,  size, birthdate));
 	}
 
 
 	@PostMapping("/registerclient")
+	@Operation(summary = "Регистрация нового клиента")
 	public ResponseEntity<Client> registerClient(@RequestBody @Valid ClientDto clientDto) {
 		return ResponseEntity.ok(clientService.registerClient(clientDto));
 	}
